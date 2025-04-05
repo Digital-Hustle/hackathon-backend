3228a1b226aeaa7db5909361174492490a35b802
KIRI4444 <kirillpopov6333@gmail.com>
2025-04-05 23:06:21 +0300

    Try to cope with parser

---
+++ src/main/java/com/example/service/FileParsingService.java
@@ -0,0 +1,12 @@
+package com.example.service;
+
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public interface FileParsingService {
+    // CompletableFuture<EnergyMetrics> parseExcelAsync(MultipartFile file);
+}
